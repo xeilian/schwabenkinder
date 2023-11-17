94c5ae784eb6feffaeae031b93e8429006b1170d
xeilian <quqiuh@gmail.com>
2023-11-17 16:15:10 +0100

    V16

--- [SK] #11 Bevölkerungsstatistik, Geonames-ID und Koordinaten extrahieren.py
+++ [SK] #11 Bevölkerungsstatistik, Geonames-ID und Koordinaten extrahieren.py
@@ -1,34 +1,27 @@
 #11 | GEONAMES-ID UND KOORDINATEN EXTRAHIEREN (FÜR BEVÖLKERUNGSSTATISTIK)
-#Voraussetzung: Excel-Tabelle mit drei Spalten. A = Name, B = Land, C = ADM-Code 1 --> Code legt ID via API in D ab
+#Voraussetzung: Excel-Tabelle mit mind. einer Spalte mit Gemeindenamen --> Code legt ID und Koordinaten in G ab
 
 import pandas as pd
 import requests
 
 # Lese die Excel-Datei
-data = pd.read_excel(f"D:\schwabenkinder\schwabenkinder_dok_admex_a.xlsx", sheet_name='Tabelle1')
-endpoint = 'http://api.geonames.org/searchJSON'
-counter = 0
-api_key_index = 0
-api_keys = ['xeilian', 'code', 'brain', 'world', 'six', 'power', 'cool', 'help', 'jackie', 'nice', 'mind', 'sing', 'dive', 'time', 'money', 'california', 'doggy', 'free', 'bird', 'radio', 'swift', 'brian', 'heavy', 'value', 'stop', 'happy', 'moin', 'light']
+data = pd.read_excel(r"C:\Users\quqiu\PycharmProjects\schwabenkinder\schwabenkinder_bevölkerungsstatistik_1880.xlsx", sheet_name='Bevölkerungsstatistik 1880')
 
 # Funktion zum Abfragen der Geonames API
-def query_geonames_api(place, country, adm1):
+def query_geonames_api(place):
     endpoint = 'http://api.geonames.org/searchJSON'
-    api_keys = ['xeilian', 'code', 'brain', 'world', 'six', 'power', 'cool', 'help', 'jackie', 'nice', 'swift', 'brian',
-                'heavy', 'value', 'stop', 'happy', 'moin', 'light']
-    api_key = api_keys[api_key_index]
+    api_key = 'power'
 
     # API-Anfrage senden
     params = {
         "q": place,
-        "country": country,
-        "adminCode1": adm1,
+        "country": "AT",
         "maxRows": 1,
-        "featureClass": "P",
+        "featureCode": "ADM3",
         "username": api_key
     }
 
-    print(place, country, adm1)
+    print(place)
     response = requests.get(endpoint, params=params)
     print(f'API-Anfrage für {place} gesendet')
 
@@ -39,6 +32,8 @@ def query_geonames_api(place, country, adm1):
             result = data['geonames'][0]
             return {
                 'geonameId': result.get('geonameId', ''),
+                'lat': result.get('lat', ''),
+                'lon': result.get('lng', '')
             }
 
     return None
@@ -46,25 +41,17 @@ def query_geonames_api(place, country, adm1):
 
 # Schleife über die Datensätze in der Excel-Datei
 for index, row in data.iterrows():
-    place = row.iloc[0]  # Verwende Spalte A
-    country = row.iloc[1]  # Verwende Spalte B
-    adm1 = row.iloc[2]  # Verwende Spalte C
+    place = row.iloc[2]  # Verwende Spalte C
 
     # Überprüfen, ob alle benötigten Informationen vorhanden sind
-    if pd.notnull(place) and pd.notnull(country) and pd.notnull(adm1):
-        result = query_geonames_api(place, country, adm1)
+    if pd.notnull(place):
+        result = query_geonames_api(place)
         print(result)
 
         if result is not None:
             # Ergebnisse in die Excel-Datei eintragen
-            data.loc[index, "Spalte D"] = str(result)
-
-    counter += 1
-    print(counter)
-
-    if counter & 900 == 0:
-        api_key_index = (api_key_index + 1) % len(api_keys)
+            data.loc[index, "Spalte G"] = str(result)
 
 # Ergebnisse in eine neue Excel-Datei schreiben
-data.to_excel(f"D:\schwabenkinder\schwabenkinder_dok_admex_a.xlsx", sheet_name="Tabelle1", index=False)
+data.to_excel(r"C:\Users\quqiu\PycharmProjects\schwabenkinder\schwabenkinder_bevölkerungsstatistik_1880.xlsx", sheet_name="Bevölkerungsstatistik 1880", index=False)
 print('Vorgang abgeschlossen.')
